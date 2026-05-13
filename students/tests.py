dce7ad2ef9c6f160d2e5e3b4074672ca472b8107
dev-lumpy <andres.luna@uab.edu.bo>
2026-05-13 12:33:30 -0400

    Aprendiendo los tests

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class UrlTests(TestCase):
+
+    def test_test_api_route(self):
+        response = self.client.get("/students/test/")
+        self.assertEqual(response.status_code, 200)
